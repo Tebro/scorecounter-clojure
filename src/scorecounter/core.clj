a6074267534e763df645045be498ab8fac0f107e
Richard Weber <riche.weber@gmail.com>
2017-02-13 18:44:51 +0200

    Documentation

--- src/scorecounter/core.clj
+++ src/scorecounter/core.clj
@@ -2,32 +2,42 @@
   (:gen-class))
 
 
-(defn add-player [players playerName]
-    (assoc players playerName 0))
+(defn add-player 
+  "Adds a player to the provided map of players" 
+  [players playerName] 
+  (assoc players playerName 0))
 
-(defn add-score [players name]
+(defn add-score 
+  "Add a point to the player identified by name in the players map"
+  [players name]
   (if (contains? players name)
       (update-in players [name] inc)
       (do (println "No such player") players)))
 
-(defn print-scores [players]
+(defn print-scores 
+  "Prints out all the players in players and their scores"
+  [players]
   (doseq [x players] (println (x 0) ":" (x 1))))
 
 
-(defn setup [players]
-    (println "Enter name: (empty to move on)")
-    (let [in (read-line)]
-      (if (empty? in)
-        players
-        (recur (add-player players in)))))
+(defn setup 
+  "Starts an interactive loop for fillin in players into the provided map" 
+  [players]
+  (println "Enter name: (empty to move on)")
+  (let [in (read-line)]
+    (if (empty? in)
+      players
+      (recur (add-player players in)))))
 
-(defn game [players]
-    (print-scores players)
-    (println "Enter name to add score (empty to end)")
-    (let [in (read-line)]
-      (if (empty? in)
-        players
-        (recur (add-score players in)))))
+(defn game 
+  "Starts interactive loop to add score to players in the provided map"
+  [players]
+  (print-scores players)
+  (println "Enter name to add score (empty to end)")
+  (let [in (read-line)]
+    (if (empty? in)
+      players
+      (recur (add-score players in)))))
 
 (defn -main
   "The game starts here"
